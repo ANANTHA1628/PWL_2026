0bb5396ebc1265311d9a3dbdffa2cebbbbaf3f38
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2026-02-25 19:08:14 +0700

    percobaan 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+   return 'Hello World';
 });
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/', function () {
+   return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+   return '244107020127|Aryaputra Ferdiaanta';
+});
+
+Route::get('/user/{nanta}', function ($name) {
+   return 'Nama saya '.$name;
+   });
+   
+   Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) 
+   { return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;});
+   
+   Route::get('/articles/{id}', function ($id) 
+   { return 'Halaman artikel dengan ID ' .$id; });
+
+   Route::get('/user/{name?}', function ($name='John') {
+      return 'Nama saya '.$name;
+      });
+      
+      
