d93a5ab108dcb635d525ec2e54855f70219d616f
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2026-02-25 19:51:30 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/hello', function () {
-   return 'Hello World';
-});
+Route::resource('photos', PhotoController::class);
+
+Route::get('/hello', [PageController::class, 'hello']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles /{id}', [ArticleController::class, 'articles']);
 
 Route::get('/world', function () {
    return 'World';
@@ -31,5 +40,14 @@ Route::get('/user/{nanta}', function ($name) {
    Route::get('/user/{name?}', function ($name='John') {
       return 'Nama saya '.$name;
       });
+      
+      Route::resource('photos', PhotoController::class)->only([
+         'index', 'show'
+     ]);
+     
+     Route::resource('photos', PhotoController::class)->except([
+         'create', 'store', 'update', 'destroy'
+     ]);
+     
       
       
